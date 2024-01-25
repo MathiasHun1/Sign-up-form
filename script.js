d52b46333eb241c3747f1640f8834115cecb3cac
MathiasHun <mathias.hun@gmail.com>
2024-01-25 18:00:58 +0100

    create a simple validation process with js

---
+++ script.js
@@ -0,0 +1,57 @@
+const nameInput = document.querySelector('#user_name');
+const namePattern = /^[A-Za-zÁáÉéÍíÓóÖöŐőÚúÜüŰű\s]+$/
+const emailInput = document.querySelector('#user_email');
+const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const passwordInput = document.querySelector('#user_password');
+const passwordPattern = /[A-Z][a-z]*\d.*\d\W/;
+const confirmInput = document.querySelector('#pasword_confirm');
+
+
+nameInput.addEventListener('blur', () => {
+    if (namePattern.test(nameInput.value)) {
+        nameInput.style.backgroundColor = 'rgb(31, 237, 45)';
+    } else {
+        nameInput.style.backgroundColor = 'red';
+
+    }
+})
+
+emailInput.addEventListener('blur', () => {
+    if (emailPattern.test(emailInput.value)) {
+        emailInput.style.backgroundColor = 'rgb(31, 237, 45)';
+    } else {
+        emailInput.style.backgroundColor = 'red';
+    }
+})
+
+passwordInput.addEventListener('blur', ()=> {
+    if (passwordPattern.test(passwordInput.value)) {
+        passwordInput.style.backgroundColor = 'rgb(31, 237, 45)';
+    } else {
+        passwordInput.style.backgroundColor = 'red';
+
+    }
+})
+
+confirmInput.addEventListener('blur', () => {
+    if (passwordInput.value === confirmInput.value) {
+        confirmInput.style.backgroundColor = 'rgb(31, 237, 45)';
+    } else {
+        confirmInput.style.backgroundColor = 'red';
+    }
+})
+
+
+
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+});
+
+
+
+
+
+
+
+
+// /[A-Z][a-z]*\d.*\d\W/
